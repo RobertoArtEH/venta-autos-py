9b1d03dee9e3d9239af3ad87c5b86354eb29e58b
Roberto Esqueda <roberto.raeh@gmail.com>
2020-01-26 18:57:38 -0600

    Interface class improvements

--- Interface.py
+++ Interface.py
@@ -6,20 +6,14 @@ class Interface():
     def __init__(self):
         self.sales = Ventas()
 
-    def askSale(self):
-        return input('¿Agregar un automóvil? (s/n): ')
-
-    def askEmployee(self):
-        return input('¿Deseas agregar un nuevo empleado? (s/n): ')
-
     def employeeSales(self, employee):
-        answer = self.askSale()
+        answer = input('¿Agregar un automóvil? (s/n): ')
         while not answer == 'n':
             brand = input('Ingresa la marca del Automovil: ')
             model = input('Ingresa el modelo del Automovil: ')
             price = float(input('Ingresa el precio del Automovil: '))
             employee.newSale(brand, model, price)
-            answer = self.askSale()
+            answer = input('¿Agregar un automóvil? (s/n): ')
 
     def printSalary(self, employee):
         employee.finalSalary()
@@ -36,9 +30,9 @@ class Interface():
             print('Marca: ' + car['brand'])
             print('Modelo: ' + car['model'])
             print('Precio:', car['price'])
-            print('-----------------')
+            print('****')
 
-    def newEmployee(self):
+    def addNewEmployee(self):
         name = input('Ingresa nombre de empleado: ')
         employee = Empleado(name)
 
@@ -47,21 +41,21 @@ class Interface():
         self.printSalary(employee)
 
     def askNewEmployee(self):
-        answer = self.askEmployee()
+        answer = input('¿Deseas agregar un nuevo empleado? (s/n): ')
         while not answer == 'n':
-            self.newEmployee()
-            answer = self.askEmployee()
+            self.addNewEmployee()
+            answer = input('¿Deseas agregar un nuevo empleado? (s/n): ')
 
     def printEmployees(self):
-        print('---------------------')
+        print('****')
         print('Lista de empleados:')
 
         for name in self.sales.getNames():
             print(name)
 
     def printNomina(self):
-        print('---------------------')
-        print('Nomina: ${0}'.format(self.sales.getNomina()))
+        print('****')
+        print('\x1b[6;30;42m' + '* Nomina: ${0}'.format(self.sales.getNomina()) + '\x1b[0m')
 
     def loadFile(self):
         try:
@@ -80,7 +74,7 @@ class Interface():
 
     def run(self):
         self.loadFile()
-        self.newEmployee()
+        self.addNewEmployee()
         self.askNewEmployee()
         self.printEmployees()
         self.printNomina()
